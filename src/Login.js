4d9bccd0ea4c3326c74bb04b6fcd53637fb6e584
ta2bey371 <ta2bey371@gmeil.com>
2026-02-18 19:01:32 +0900

    ログイン画面を編集しました

--- src/Login.js
+++ src/Login.js
@@ -1,19 +1,30 @@
 import React, { useState } from 'react'
 
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Header from './components/Header';
+import "./Login.css";
 
 
 const Login = ({catchName}) => {
   const [userName,setUserName] = useState("");
   const getUserName = (e) => setUserName(e.target.value);
+  const navigate = useNavigate();
+  const allow = () => {
+    if(userName.trim() !== "") {
+      catchName(userName);
+      navigate('/Home');
+    }else alert("ユーザー名が入力されていません。");
+  }
+
 
   return (
     <>
       <Header name={""}/>
-      <div>Login</div>
-      <input type='text' onChange={getUserName} id='nameArea'/>
-      <Link to='/Home' onClick={catchName(userName)}>login</Link>
+      <div className='content'>
+        <p>Login</p>
+        <input type='text' onChange={getUserName} id='nameArea' placeholder='ユーザー名'/>
+        <button onClick={allow}>アクセス</button>
+      </div>
     </>
   )
 }
