75009797f7641419d5be35d4eae8fe1ec5a89243
ta2bey371 <ta2bey371@gmeil.com>
2026-02-19 00:36:55 +0900

    フッターの機能を作りました。

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import { useState } from "react";
+import "./Footer.css"
 
 const Footer = () => {
+  // creditsを書き換えると表示内容が変わります。
+  const credits = [
+    {"name":"ta2bey371","text":"hello world!"},
+    {"name":"test_name","text":"test_text"},
+    {"name":"test_name","text":"test_text"},
+  ];
+  const [showId,setShowId] = useState(null);
   return (
-    <div>Footer</div>
+    <>
+    <div onClick={()=>setShowId(null)}>
+      hello 
+    </div>
+    <dl>
+      {credits.map((credit,index)=>{
+        const showControre = (index) => {
+          if(showId===index)setShowId(null);
+          else setShowId(index);
+        }
+        return (
+          <div key={index}>
+            <dt onClick={()=>showControre(index)}>{credit.name}</dt>
+            {showId === index?<dd>{credit.text}</dd>:<dd style={{display:"none"}}></dd>}
+          </div>
+        );
+      })}
+    </dl>
+    </>
   )
 }
 
